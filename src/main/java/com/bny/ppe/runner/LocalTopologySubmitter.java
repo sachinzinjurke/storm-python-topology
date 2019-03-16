687d286fbe76fe2eaf6d923755d3244becc2706a
Sachin Zinjurke <sachinzinjurke@gmail.com>
2019-03-16 18:05:26 +0530

    Adding git storm-python-topology module

---
+++ src/main/java/com/bny/ppe/runner/LocalTopologySubmitter.java
@@ -0,0 +1,95 @@
+package com.bny.ppe.runner;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bny.common.storm.topology.SingleTopologySubmission;
+import com.bny.common.storm.topology.TopologySubmission;
+
+public final class LocalTopologySubmitter {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LocalTopologySubmitter.class.getName());
+
+	private LocalTopologySubmitter() {
+	}
+
+	private static void validateArgs(final String[] args) {
+		if(args==null || args.length==0){
+			throw new IllegalArgumentException(
+					"Arguments missing: 1: Context XML was not defined, Argument 2: Topology name was not defined"); 
+		}
+		if(args[0] == null) {
+			throw new IllegalArgumentException(
+					"Argument 1: Context XML was not defined"); 
+		};
+		if(args[1] == null) {
+			throw new IllegalArgumentException(
+					"Argument 2: Topology name was not defined"); 
+		};
+	}
+
+	private void submitTopologies(final LocalCluster cluster, final TopologySubmission topologySubmission, String topologyName) {
+		LOGGER.info("MP-CONTENT-OFFER :: Submit MP-CONTENT-OFFER Topology");
+		for (Map.Entry<String, StormTopology> entry : topologySubmission.getStormTopologies().entrySet()) {
+			if(StringUtils.equalsIgnoreCase(topologyName, entry.getKey())){
+				StormTopology stormTopology = entry.getValue();
+				cluster.submitTopology(entry.getKey(), topologySubmission.getConfig(), stormTopology);
+				LOGGER.info("Successfully submitted topology :: "+entry.getKey());
+			}		
+		}
+	}
+	
+	private void killTopologies(final LocalCluster cluster,	final TopologySubmission topologySubmission) {
+		for (String key : topologySubmission.getStormTopologies().keySet()) {
+			cluster.killTopology(key);
+		}
+	}
+
+	public static void main(final String[] args) {
+		validateArgs(args);
+		final String context_xml = args[0]; //rt-mp-content-offer-context.xml
+		final String topologyName = args[1]; //mpContentOfferLoaderTopology
+		
+		final ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(context_xml);
+
+		final TopologySubmission topologySubmission = (TopologySubmission) context.getBean(SingleTopologySubmission.class);
+		final Integer runtime = NumberUtils.toInt("10000000");
+		LOGGER.info("STORM PYTHON TOPOLOGY :: Before cluster started");
+		final LocalCluster cluster = new LocalCluster();		
+		final LocalTopologySubmitter localTopologySubmitter = (LocalTopologySubmitter) context.getBean("localTopologySubmitter");
+		localTopologySubmitter.submitTopologies(cluster, topologySubmission, topologyName);
+
+		/*try {
+			submitTopologies(topologySubmission);
+		} catch (AlreadyAliveException e) {
+			// TODO Auto-generated catch block
+			LOGGER.error("error:", e);
+		} catch (InvalidTopologyException e) {
+			// TODO Auto-generated catch block
+			LOGGER.error("error:", e);
+		}*/
+
+		LOGGER.info("*************************Cluster Started*************************");
+		Utils.sleep(runtime); // Let run for a bit
+		try {
+			Thread.sleep(runtime);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info("*************************Started Cluster Shutdown*************************");
+		localTopologySubmitter.killTopologies(cluster, topologySubmission);
+		cluster.shutdown();
+		context.close();
+		LOGGER.info("*************************Cluster Shutdown Completed*************************");
+	}
+
+}
---
+++ src/main/java/com/bny/ppe/spouts/RandomSentenceSpout.java
@@ -0,0 +1,120 @@
+package com.bny.ppe.spouts;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.IRichSpout;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bny.common.storm.components.spouts.GenericSpout;
+import com.bny.ppe.bolts.SplitSentence;
+
+public class RandomSentenceSpout extends GenericSpout implements IRichSpout {
+
+	private static final Logger logger = LoggerFactory.getLogger(RandomSentenceSpout.class.getName());
+
+	SpoutOutputCollector _collector;
+	Random _rand;
+
+	public RandomSentenceSpout() {
+
+	}
+
+	public RandomSentenceSpout(String componentId) {
+		super(componentId);
+	}
+
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		// TODO Auto-generated method stub
+		_collector = collector;
+		_rand = new Random();
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void activate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deactivate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void nextTuple() {
+		// TODO Auto-generated method stub
+		//Utils.sleep(100);
+		String[] sentences = new String[] { sentence("the cow jumped over the moon"),
+				sentence("an apple a day keeps the doctor away"), sentence("four score and seven years ago"),
+				sentence("snow white and the seven dwarfs"), sentence("i am at two with nature") };
+		final String sentence = sentences[_rand.nextInt(sentences.length)];
+
+		logger.info("Emitting tuple: {}", sentence);
+
+		_collector.emit(new Values(sentence));
+	}
+
+	protected String sentence(String input) {
+		return input;
+	}
+
+	public void ack(Object msgId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void fail(Object msgId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("sentence"));
+
+	}
+
+	public Map<String, Object> getComponentConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addToTopology(TopologyBuilder builder) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// Add unique identifier to each tuple, which is helpful for debugging
+	public static class TimeStamped extends RandomSentenceSpout {
+		private final String prefix;
+
+		public TimeStamped() {
+			this("");
+		}
+
+		public TimeStamped(String prefix) {
+			this.prefix = prefix;
+		}
+
+		protected String sentence(String input) {
+			return prefix + currentDate() + " " + input;
+		}
+
+		private String currentDate() {
+			return new SimpleDateFormat("yyyy.MM.dd_HH:mm:ss.SSSSSSSSS").format(new Date());
+		}
+	}
+}
